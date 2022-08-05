2a42a615a9fff3134ea63e52edf0591bb4d31a55
Student User <student@workstation.lab.example.com>
2022-08-05 11:44:22 -0400

    Updated

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version 1 new app  of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is version 1.2  new app  of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
